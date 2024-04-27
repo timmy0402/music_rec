8c90fe06bc48ac1ccf8d1a122f3b37d246e96821
timmy0402 <minhnhathb04@gmail.com>
2024-04-27 14:54:36 -0700

    added connection to python file through Flask

--- output.py
+++ output.py
@@ -1,16 +1,9 @@
-from flask import request, Flask
-import requests
-
-#setting flask framework
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/',methods=['GET', 'POST'])
-def setUp():
-    return render_template('index.html') 
+@app.route('/run_python', methods=['POST'])
+def run_python():
+    return request.data
 
-@app.route('/process',methods=['POST'])
-def process():
-    return "hello"
-
-if (__name__ == "__main__"):
-    app.run()
+if __name__ == '__main__':
+    app.run(port=5000)
